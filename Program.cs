fae9ae97c02585b5db972bf86f2e225b9f895110
Luis <Luis@DESKTOP-V5LQVFF>
2025-01-28 11:40:58 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exeptions_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gib eine Zahl ein");
+            string zahl1 = Console.ReadLine();
+            Console.WriteLine("Gib noch eine Zahl ein");
+            string zahl2 = Console.ReadLine();
+            try
+            {
+                double Zahl1 = Convert.ToDouble(zahl1);
+                double Zahl2 = Convert.ToDouble(zahl2);
+                double Zahl3 = Zahl1 / Zahl2;
+                Console.WriteLine(Zahl3);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Das war keine Zahl");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Man kann nicht durch 0 teilen");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Mathematischer Fehler");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Arbeitsspeicher Voll");
+            }
+            finally
+            {
+                Console.WriteLine("Fertig");
+            }
+        }
+    }
+}
